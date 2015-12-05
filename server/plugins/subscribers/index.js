1e35d4c53366af7342ebbcef9cfacaa37b1b7207
Kurtis Kemple <kurtiskemple@gmail.com>
2015-12-05 14:12:12 -0500

    add email to sign up for beta

--- server/plugins/subscribers/index.js
+++ server/plugins/subscribers/index.js
@@ -1,4 +1,6 @@
 import Joi from 'joi'
+import mg from 'nodemailer-mailgun-transport'
+import nodemailer from 'nodemailer'
 import request from 'superagent'
 
 import logger from '../../../logger'
@@ -7,6 +9,34 @@ const mailchimpDomain = process.env.MAILCHIMP_API_DOMAIN
 const mailchimpBetaList = process.env.MAILCHIMP_BETA_LIST_ID
 const mailchimpUrl = `https://${mailchimpDomain}.api.mailchimp.com/3.0`
 const betaListUrl = `/lists/${mailchimpBetaList}/members`
+
+const auth = {
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+  },
+}
+
+const mailer = nodemailer.createTransport(mg(auth))
+
+const sendMail = (messageConfig) => {
+  mailer.sendMail(messageConfig, (err, info) => {
+    if (err) logger.error(err)
+    logger.info(info, 'mailgun response')
+  })
+}
+
+const signUpText = `
+Hey there!
+
+Thank you for requesting an invite to Skynet AI! We will be sending out the first batch soon!
+
+In the meantime, feel free to share with your friends!
+http://skynet.releasable.io
+
+Cheers,
+The Skynet AI Team
+`
 
 export const register = (server, options, next) => {
   server.route([
@@ -36,8 +66,18 @@ export const register = (server, options, next) => {
                   success: false,
                   message: err.message,
                 })
+
                 return
               }
+
+              const messageConfig = {
+                to: email,
+                from: 'no-reply@skynet.releasable.io',
+                subject: `Skynet - Thank you for requesting an invite!`,
+                text: signUpText,
+              }
+
+              sendMail(messageConfig)
 
               reply({
                 success: true,
